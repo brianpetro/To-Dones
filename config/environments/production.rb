444898a2fffb83c08085a2afa0c341b5bfbc5452
brianpetro <brian@plexm.com>
2013-03-17 13:22:18 -0400

    initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Brianpetro::Application.configure do
+Todone::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
